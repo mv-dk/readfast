0f525323e9e1aa1573d7d94c12f10dc99cdc9c43
Martin Vestergaard <mvestergaard@gmail.com>
2016-04-26 16:55:21 +0200

    Added keyboard control. Added progress bar.

--- readfast.js
+++ readfast.js
@@ -19,6 +19,8 @@ var pauseBtn = elem("pauseBtn");
 var nextParagraphBtn = elem("nextParagraphBtn");
 var decreaseSpeedBtn = elem("decreaseSpeedBtn");
 var increaseSpeedBtn = elem("increaseSpeedBtn");
+var progressBar = elem("progressBar");
+var pause = true;
 
 startBtn.onclick = startBtnClicked;
 clearBtn.onclick = clearBtnClicked;
@@ -30,6 +32,31 @@ nextParagraphBtn.onclick = nextParagraphBtnClicked;
 decreaseSpeedBtn.onclick = decreaseSpeedBtnClicked;
 increaseSpeedBtn.onclick = increaseSpeedBtnClicked;
 
+document.onkeydown = function(e) {
+	//console.log("keyCode: "+e.keyCode);
+	if (e.keyCode == 32) { //space
+		if (pause) {
+			startBtnClicked();
+		} else {
+			pauseBtnClicked();
+		}
+	}
+	else if (e.keyCode == 37) { // left
+		prevParagraphBtnClicked();
+	}
+	else if (e.keyCode == 39) { // right
+		nextParagraphBtnClicked();
+	}
+	else if (e.keyCode == 38) { // up
+		increaseSpeedBtnClicked();
+	}
+	else if (e.keyCode == 40) { // down
+		decreaseSpeedBtnClicked();
+	}
+}
+
+pauseBtn.style.display = "none";
+
 NORMAL_PAUSE = 400;
 COMMA_PAUSE = 600;
 PERIOD_PAUSE = 800;
@@ -39,40 +66,52 @@ var state = {
     wordArray: undefined,
     idx: 0,
     current: function(){ 
-	return this.wordArray[this.idx]; 
+		return this.wordArray[this.idx]; 
     },
     next: function() {
-	if (this.wordArray == undefined || this.idx >= this.wordArray.length) return undefined;
-	return this.wordArray[this.idx++];
+		if (this.wordArray == undefined || this.idx >= this.wordArray.length) return undefined;
+		return this.wordArray[this.idx++];
     },
     nextWord: function() {
-	if (this.idx+1 >= this.wordArray.length) { return undefined; }
-	return this.wordArray[this.idx+1];
+		if (this.idx+1 >= this.wordArray.length) { return undefined; }
+		return this.wordArray[this.idx+1];
     },
     prevSentence: function(){
-	var i = this.idx-2;
-	while (i > 0) {
-	    var word = wordArray[i];
-	    if (word.endsWith(".")) {
-		this.idx = i + 1;
-		break;
-	    }
-	    i--;
-	}
-	if (i == 0) this.idx = 0;
+		var i = this.idx-2;
+		while (i > 0) {
+			var word = wordArray[i];
+			if (endsWithStop(word)) {
+				this.idx = i + 1;
+				break;
+			}
+			i--;
+		}
+		if (i == 0) this.idx = 0;
     },
     nextSentence: function(){
-	var i = this.idx;
-	var word = this.next();
-	if (word == undefined) return;
-	while (!word.endsWith(".")) {
-	    word = this.next();
-	}
+		var i = this.idx;
+		var word = this.next();
+		if (word == undefined) return;
+		while (!endsWithStop(word)) {
+			word = this.next();
+		}
     }
 };
 
+function updateProgressBar(){
+	var p = 0;
+	if (state.wordArray != undefined) { 
+		p = 100.0 * (state.idx / state.wordArray.length);
+	}
+	setProgressBarPct(p);
+}
+
+function setProgressBarPct(percentage){
+	progressBar.style.width = percentage+"%";
+}
+
 function clearBtnClicked(){
-    textArea.textContent = "";
+    textArea.value = "";
 }
 
 function loadBtnClicked(){
@@ -90,31 +129,36 @@ function load(){
 
 function startBtnClicked() {
     if (textAreaIsDirty) {
-	load();
+		load();
     }
     pause = false;
+	
+	startBtn.style.display="none";
+	pauseBtn.style.display="inline-block";
+
     play();
 }
 
 function wpmToMs(wpm){
     /*
-    var wps = wpm / 60;
-    var wpms = wps / 1000;
-    var msBreak= 1 / wpms;
-    return msBreak;
+      var wps = wpm / 60;
+      var wpms = wps / 1000;
+      var msBreak= 1 / wpms;
+      return msBreak;
     */
     return 60000 / wpm;
 }
 
 function play(){
     if (pause) { return; }
+	updateProgressBar();
     var word = state.next();
     if (word == undefined) return;
     var t = getPausePeriod(word);
     
     put(wordArea, word);
     setTimeout(function (){
-	play();
+		play();
     }, t);
 }
 
@@ -126,22 +170,25 @@ function rewindToBeginningBtnClicked() {
 function prevParagraphBtnClicked() {
     state.prevSentence();
     put(wordArea, state.current());
+	updateProgressBar();
 }
-
-var pause = false;
 
 function pauseBtnClicked() {
     pause = true;
+	
+	startBtn.style.display="inline-block";
+	pauseBtn.style.display="none";
 }
 
 function nextParagraphBtnClicked() {
     state.nextSentence();
     put(wordArea, state.current());
+	updateProgressBar();
 }
 function decreaseSpeedBtnClicked() {
     var n = Number(speedInput.value);
     if (n > 10) {
-	speedInput.value = n - 10;
+		speedInput.value = n - 10;
     }
 }
 function increaseSpeedBtnClicked() {
@@ -161,102 +208,112 @@ function getWordArray(fullText){
     var spaceSplit = fullText.split(" ");
     var fullArr = [];
     spaceSplit.forEach(function (x) {
-	var nSplit = x.split("\n");
-	nSplit.forEach(function (y) {
-	    var rSplit = y.split("\r");
-	    rSplit.forEach(function (elem){
-		if (elem != "") { fullArr.push(elem); }
-	    });
-	});
+		var nSplit = x.split("\n");
+		nSplit.forEach(function (y) {
+			var rSplit = y.split("\r");
+			rSplit.forEach(function (elem){
+				if (elem != "") { fullArr.push(elem); }
+			});
+		});
     });
     return fullArr;
+}
+
+function endsWithStop(word) {
+	return word.endsWith(".") ||
+		word.endsWith("!") ||
+		word.endsWith("?") ||
+		word.endsWith(".\"") ||
+		word.endsWith("!\"") ||
+		word.endsWith("?\"") ||
+		word.endsWith("...");
 }
 
 function getPausePeriod(word){
     var f = 1;
     if (word.endsWith(",") ||
         (word.length > 8 && word.length <= 12)) f = 1.5;
-    if (word.endsWith(".") ||
-	word.endsWith("!") ||
-	word.endsWith("?") ||
+	if (endsWithStop(word) ||
         word.endsWith(")") || 
-        word.endsWith("(") ||
-        word.length > 12) f = 1.8;
+        word.endsWith("(")) 
+	{
+		f = 1.8;
+	}
     return wpmToMs(speedInput.value) * f;
     /*
-    if (word.endsWith(",")) { return COMMA_PAUSE; };
-    if (word.endsWith(".")) { return PERIOD_PAUSE; };
-    return NORMAL_PAUSE;
+      if (word.endsWith(",")) { return COMMA_PAUSE; };
+      if (word.endsWith(".")) { return PERIOD_PAUSE; };
+      return NORMAL_PAUSE;
     */
 }
 
 
 (function test(){
     var assertEquals = function(actual, expected){
-	if (actual != expected){
-	    var caller = arguments.callee.caller.name;
-	    throw "FAIL, expected '"+expected+"', actual '"+actual+"' at "+caller;
-	}
+		if (actual != expected){
+			var caller = arguments.callee.caller.name;
+			throw "FAIL, expected '"+expected+"', actual '"+actual+"' at "+caller;
+		}
     }
     var assertNotUndefined = function(val){
-	if (val == undefined) {
-	    var caller = arguments.callee.caller.name;
-	    throw "FAIL, value was undefined at "+caller;
-	}
+		if (val == undefined) {
+			var caller = arguments.callee.caller.name;
+			throw "FAIL, value was undefined at "+caller;
+		}
     }
     
     !function test_canSplitWords (){
-	var wordArray = getWordArray("abc def");
-	assertNotUndefined(wordArray);
-	assertEquals(wordArray[0], "abc");
-	assertEquals(wordArray[1], "def");
+		var wordArray = getWordArray("abc def");
+		assertNotUndefined(wordArray);
+		assertEquals(wordArray[0], "abc");
+		assertEquals(wordArray[1], "def");
     }();
 
     !function test_canSplitWordsOnLineBreak(){
-	var wordArray = getWordArray("abc\ndef");
-	assertNotUndefined(wordArray);
-	assertEquals(wordArray[0], "abc");
-	assertEquals(wordArray[1], "def");
+		var wordArray = getWordArray("abc\ndef");
+		assertNotUndefined(wordArray);
+		assertEquals(wordArray[0], "abc");
+		assertEquals(wordArray[1], "def");
     }();
 
     !function test_canSplitWordsOnReturn(){
-	var wordArray = getWordArray("abc\rdef");
-	assertNotUndefined(wordArray);
-	assertEquals(wordArray[0], "abc");
-	assertEquals(wordArray[1], "def");
+		var wordArray = getWordArray("abc\rdef");
+		assertNotUndefined(wordArray);
+		assertEquals(wordArray[0], "abc");
+		assertEquals(wordArray[1], "def");
     }();
 
     !function test_noEmptyElements(){
-	var wordArray = getWordArray("abc  def");
-	assertNotUndefined(wordArray);
-	assertEquals(2, wordArray.length);
-	assertEquals(wordArray[0], "abc");
-	assertEquals(wordArray[1], "def");
+		var wordArray = getWordArray("abc  def");
+		assertNotUndefined(wordArray);
+		assertEquals(2, wordArray.length);
+		assertEquals(wordArray[0], "abc");
+		assertEquals(wordArray[1], "def");
     }();
 
     !function test_noEmptyElements2(){
-	var wordArray = getWordArray("abc\r\ndef");
-	assertNotUndefined(wordArray);
-	assertEquals(2, wordArray.length);
-	assertEquals(wordArray[0], "abc");
-	assertEquals(wordArray[1], "def");
+		var wordArray = getWordArray("abc\r\ndef");
+		assertNotUndefined(wordArray);
+		assertEquals(2, wordArray.length);
+		assertEquals(wordArray[0], "abc");
+		assertEquals(wordArray[1], "def");
     }();
 
     /*
-    !function test_pauseNormalWord(){
-	var p = getPausePeriod("huba");
-	assertEquals(p, NORMAL_PAUSE);
-    }();
+      !function test_pauseNormalWord(){
+	  var p = getPausePeriod("huba");
+	  assertEquals(p, NORMAL_PAUSE);
+      }();
 
-    !function test_pauseCommaWord(){
-	var p = getPausePeriod("huba,");
-	assertEquals(p, COMMA_PAUSE);
-    }();
+      !function test_pauseCommaWord(){
+	  var p = getPausePeriod("huba,");
+	  assertEquals(p, COMMA_PAUSE);
+      }();
 
-    !function test_pausePeriodWord(){
-	var p = getPausePeriod("huba.");
-	assertEquals(p, PERIOD_PAUSE);
-    }();
+      !function test_pausePeriodWord(){
+	  var p = getPausePeriod("huba.");
+	  assertEquals(p, PERIOD_PAUSE);
+      }();
     */
 })();
 
